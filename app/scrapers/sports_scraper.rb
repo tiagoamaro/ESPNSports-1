04e68475cd7dce3a32579052be1f65234255ec62
Tiago Amaro <tiagocis@gmail.com>
2015-04-16 19:42:31 -0300

    NBA and MLB have JavaScript interface and they make reference of another games in their header, making it impossible to global scrape for ?gameId. Using its data-data attribute to pick up the correct game IDs

--- app/scrapers/sports_scraper.rb
+++ app/scrapers/sports_scraper.rb
@@ -3424,11 +3424,20 @@ class SportsScraper
             }
 
           else
-            gameIds = parser.inner_html.scan(/\?gameId=(\d+)/).each { |gId|
+
+            # MLB and NBA have a JavaScript interface
+            if @entrypoint['LeagueName'] == 'MLB' || @entrypoint['LeagueName'] == 'NBA'
+              game_ids_on_data_attribute = parser.xpath("//div[@id='scoreboard-page']").attribute('data-data').value
+              game_ids_to_parse = game_ids_on_data_attribute.scan(/\?gameId=(\d+)/)
+            else
+              game_ids_to_parse = parser.inner_html.scan(/\?gameId=(\d+)/)
+            end
+
+            gameIds = game_ids_to_parse.each do |gId|
                 if not  @to_traverse.include? gId[0] and is_number(gId[0]) then
                   @to_traverse.push(gId[0])
                 end
-            }
+            end
       
           end
 
